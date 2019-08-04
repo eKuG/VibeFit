c3b8937f8b901e66df30295be69cd4228f7784bd
Shreya Prasad <shreyapd06@gmail.com>
2019-08-04 15:01:42 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://shreya:'+ encodeURIComponent('K#$29go&$') + 'project.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://shreya:K#$29go&$@project-r5xnf.mongodb.net/test?retryWrites=true&w=majority'
+};
